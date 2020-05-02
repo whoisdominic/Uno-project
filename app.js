3423bce8d9864bb3f701b126dd2fab05d733a5fd
Dominic Cobb <dominicshajaa@icloud.com>
2020-05-02 15:09:15 -0700

    finished display cards and all classes

--- app.js
+++ app.js
@@ -13,12 +13,10 @@ class Player {
 
     }
     drawCard(){
-        this.hand.push(cardPile.shift())
+        this.hand.push(cardPile.pop())
         console.log(`${this.name} draws a card`);
     }
 }
-
-
 //////////////////////////
 // Card Classes
 //////////////////////////
@@ -31,6 +29,15 @@ class Card {
     }
 }
 
+class Wild extends Card{
+    constructor(color,value,type){
+        super(color,value,type)
+        this.type = 'wild'
+        this.value = 'wild'
+        this.color = 'wild'
+    }
+}
+
 class Numbered extends Card {
     constructor(color,value,type){
         super(color,value,type)  
@@ -38,8 +45,10 @@ class Numbered extends Card {
 }
 
 class Skip extends Card {
-    constructor(color,type){
-        super(color,type)  
+    constructor(color, value, type){
+        super(color, value, type)
+        this.type = 'action'
+        this.value = 'Skip'
     }
     effect() {
         // skips the next player (changes the player orders)
@@ -47,25 +56,33 @@ class Skip extends Card {
 }
 
 class Reverse extends Card {
-    constructor(color,type){
-        super(color,type)  
+    constructor(color, value, type){
+        super(color,value,type)  
+        this.type = 'action'
+        this.color = color 
+        this.value = 'Reverse'
     }
     effect() {
         // reverses the order of the next player to play
     }
 }
 class DrawTw0 extends Card {
-    constructor(color,type){
-        super(color,type)  
+    constructor(color, value, type){
+        super(color, value,type) 
+        this.type = 'action'
+        this.color = color 
+        this.value = 'Draw Two'
     }
     effect(target) {
         // makes target player draw 2
     }
 }
 class DrawFour extends Card {
-    constructor(color,type){
-        super(type)
-        this.color = 'wild'  
+    constructor(color, value, type){
+        super(color, value,type)
+        this.type = 'action'  
+        this.color = 'wild'
+        this.value = 'Draw Four'
     }
     effect(target) {
         // makes target player draw 2
@@ -80,11 +97,37 @@ class CardFactory {
     constructor(type){
         this.type = type
     }
-    makeCards(amount){
-        
+    makeNumbered(amount,color){
+        for (let i = 0; i < amount; i++) {
+        const numbered = new Numbered(color, i, this.type);
+            cardPile.push(numbered)
+        }
+    }
+    makeWild(amount){
+        for (let i = 0; i < amount; i++) {
+            const wild = new Wild();
+                cardPile.push(wild)
+        }
+    }
+    makeSkip(color){
+        const skip = new Skip(color, 'action');
+        cardPile.push(skip)
+}
+    makeReverse(color){
+        const reverse = new Reverse(color, 'action');
+        cardPile.push(reverse)
+    }
+    makeDrawTwo(color){
+            const drawTwo = new DrawTw0(color, 'action');
+            cardPile.push(drawTwo)
+    }
+    makeDrawFour(amount){
+        for (let i = 0; i < amount; i++) {
+            const drawFour = new DrawFour();
+                cardPile.push(drawFour)
+        }
     }
 }
-
 
 //////////////////////////
 // Card Piles
@@ -96,12 +139,55 @@ const cardInPlay = []
 
 const playerOrder = []
 
+const makeDeck = () => {
+    // Numbered Cards --------------
+    new CardFactory('numbered').makeNumbered(10,'blue')
+    new CardFactory('numbered').makeNumbered(10,'yellow')
+    new CardFactory('numbered').makeNumbered(10,'red')
+    new CardFactory('numbered').makeNumbered(10,'green')
+    // Numbered Wild --------------
+    new CardFactory('wild').makeWild(4)
+    // Draw Two --------------
+    new CardFactory('action').makeDrawTwo('blue')
+    new CardFactory('action').makeDrawTwo('yellow')
+    new CardFactory('action').makeDrawTwo('green')
+    new CardFactory('action').makeDrawTwo('red')  
+    // Reverse --------------
+    new CardFactory('action').makeSkip('blue')
+    new CardFactory('action').makeSkip('yellow')
+    new CardFactory('action').makeSkip('green')
+    new CardFactory('action').makeSkip('red')  
+    // Skip Cards --------------
+    new CardFactory('action').makeSkip('blue')
+    new CardFactory('action').makeSkip('yellow')
+    new CardFactory('action').makeSkip('green')
+    new CardFactory('action').makeSkip('red')  
+    // Draw 4 --------------
+    new CardFactory('wild').makeDrawFour(4)
+}
+
+//////////////////////////
+// Players 
+//////////////////////////
+
+const dominic = new Player('Dominic');
+
 //////////////////////////
 // Game Board
 //////////////////////////
 
-const displayCard = (player) => {
+const displayCards = (player) => {
+    // Removes cards first
+    $('.current-hand').children().remove()
     // loops through the players hand & appends their cards to the DOM
+    for (let i = 0; i < player.hand.length; i++) {
+        const $card = $('<div>').addClass(`uno-card ${player.hand[i].color} `).html(`
+        <h3>${player.hand[i].color}</h3>
+        <h2>${player.hand[i].value}</h2>
+        <h2>${player.hand[i].type}</h2>
+        `)
+        $('.current-hand').append($card)
+    }
 }
 
 //////////////////////////
@@ -109,6 +195,11 @@ const displayCard = (player) => {
 //////////////////////////
 
 // Draw button
+
+const $drawButton = $('.draw').on('click', () => {
+    dominic.drawCard()
+    displayCards(dominic)
+})
 
 // im thinking the event listeners for the 
 // cards will be added when they are appended to the DOM
@@ -119,8 +210,16 @@ const displayCard = (player) => {
 // Game Start & Game Functions
 //////////////////////////
 
+makeDeck()
+
 /* a function to make all the cards in the deck and distribute them between the
 players, (will have to have a math randomizer) */
+
+const shuffle = () => {
+    cardPile.sort(() => Math.random() - 0.5);
+    cardPile.sort(() => Math.random() - 0.5);
+    cardPile.sort(() => Math.random() - 0.5);
+}
 
 // start turn function
 
@@ -139,28 +238,29 @@ will be called.
 //////////////////////////
 // Node Test Area
 //////////////////////////
+shuffle()
 
-const dominic = new Player('Dominic');
+
 console.log(dominic);
 
-const cardBlue = new Numbered('blue','9','number');
-const cardRed = new Numbered('red','5','number');
-const cardGreen = new Numbered('green','7','number');
-const cardYellow = new Numbered('yellow','3','number');
-
-cardPile.push(cardRed)
-cardPile.push(cardYellow)
-cardPile.push(cardBlue)
-cardPile.push(cardGreen)
-
 console.log(cardPile);
 
-dominic.drawCard()
+// const cardBlue = new Numbered('blue','9','number');
+// const cardRed = new Numbered('red','5','number');
+// const cardGreen = new Numbered('green','7','number');
+// const cardYellow = new Numbered('yellow','3','number');
 
-console.log(cardPile);
+// cardPile.push(cardRed)
+// cardPile.push(cardYellow)
+// cardPile.push(cardBlue)
+// cardPile.push(cardGreen)
 
+// console.log(cardPile);
 
-console.log(cardPile);
-console.log(dominic);
+// console.log(cardPile);
+
+// console.log(cardPile.length);
+// console.log(cardPile);
+// console.log(dominic);
 
 
